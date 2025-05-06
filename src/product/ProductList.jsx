5e7d6b69a77d37490e700ffecf5cfdc07177be31
Muhammad Abdul Karim <abdkarim.muh@gmail.com>
2025-05-06 18:09:11 +0700

    use effect

---
+++ src/product/ProductList.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+import Product from "./Product";
+
+export default function ProductList() {
+  const [products, setProducts] = useState([]);
+  const loaded = useRef(false);
+
+  useEffect(() => {
+    console.info("Call Use Effect");
+
+    if (loaded.current === false) {
+      fetch("/products.json")
+        .then((response) => response.json())
+        .then((data) => setProducts(data));
+    }
+
+    return () => {
+      loaded.current = true;
+      console.info("Product List Component Unmounted");
+    };
+  });
+
+  return (
+    <>
+      <h1>Product List</h1>
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+    </>
+  );
+}
