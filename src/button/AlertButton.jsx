f3360db518452cb0bca02babb9bc1442784d36d6
Muhammad Abdul Karim <abdkarim.muh@gmail.com>
2025-05-06 17:06:27 +0700

    timer

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,6 +1,11 @@
+import { useRef } from "react";
+
 export default function AlertButton({ text, message }) {
+  const counter = useRef(0);
+
   function handleClick(e) {
-    alert(message);
+    console.info(e);
+    alert(`${message} ${counter.current++}`);
   }
 
   return <button onClick={handleClick}>{text}</button>;
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         counter: "counter.html",
         note: "note.html",
         profile: "profile.html",
+        timer: "timer.html",
       },
     },
   },
